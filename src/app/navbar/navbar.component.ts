43ea21d9337c30946071bb289312470883dfb1fe
pinkisharma-ctrl <pinki.sharma@magicedtech.com>
2025-05-30 11:21:15 +0530

    [add dark mode theme]

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component ,Renderer2} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,28 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  constructor(public _router: Router){}
+    isDarkMode = false;
+
+  constructor(public _router: Router,private renderer: Renderer2){}
+
+ngOnInit(){
+  const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.isDarkMode = true;
+    }
+}
+
+ toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 
   createForm(){
       this._router.navigate(['/create']);
